d7b21a07c2d886db09c479f475a4f982559de769
Michael C. Maresco <michael.c.maresco@gmail.com>
2021-11-18 22:17:05 -0500

    first commit

---
+++ 01-Conditional-Statements/Unsolved/script.js
@@ -0,0 +1,14 @@
+// TODO: Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 50;
+
+// TODO: Write Your JavaScript Code Here
+
+if (expression1 && expression2) {
+    console.log("true,true")
+} else if (expression1 && !expression2) { // This is a "bang"
+    console.log("true,false")
+} else if )expression1 && !expression2) {
+    console.log("false,false")
+}
---
+++ 02-Iteration/Unsolved/script.js
@@ -0,0 +1,8 @@
+// TODO: Write Your JavaScript Code Here
+var students = ['Staisha', 'Nigel', 'Kareemat', 'Christina'];
+
+console.log(students.length);
+
+for(var i=0; i < students.length; i++) {
+    console.log("Welcome to hell, " + students[i] + "!");
+}
